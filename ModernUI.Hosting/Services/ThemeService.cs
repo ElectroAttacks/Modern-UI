aa21a14dce8496bb2c4830ed34eb4f01af979da6
ElectroAttacks <casparjacobweber@gmail.com>
2024-07-28 22:13:18 +0200

    Add ThemeService class with theme management properties
    Introduce ThemeService abstract class implementing IThemeService.
    Add private fields: _selectedTheme, _synchronizeAccent, _accentColor.
    Implement SelectedTheme and SynchronizeAccent properties with
    change notifications. Add PropertyChanging and PropertyChanged
    events. Add SetAccent method (not yet implemented). Add protected
    methods OnPropertyChanging and OnPropertyChanged. Declare abstract
    method OnThemeChanging for derived class implementation.

---
+++ ModernUI.Hosting/Services/ThemeService.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using ModernUI.Hosting.Contracts.Services;
+using ModernUI.Hosting.Models;
+
+namespace ModernUI.Hosting.Services;
+
+/// <summary>
+///     Represents a service that manages the theme of the application.
+/// </summary>
+public abstract class ThemeService : IThemeService
+{
+
+    private AppTheme _selectedTheme;
+
+    private bool _synchronizeAccent;
+
+    private object _accentColor;
+
+
+    /// <inheritdoc />
+    public AppTheme SelectedTheme
+    {
+        get
+        {
+            return _selectedTheme;
+        }
+        set
+        {
+            if (_selectedTheme != value)
+            {
+                OnThemeChanging(value);
+                OnPropertyChanging();
+
+                _selectedTheme = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public bool SynchronizeAccent
+    {
+        get
+        {
+            return _synchronizeAccent;
+        }
+        set
+        {
+            if (_synchronizeAccent != value)
+            {
+                OnPropertyChanging();
+
+                _synchronizeAccent = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    /// <inheritdoc />
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    public void SetAccent(object accentColor) => throw new System.NotImplementedException();
+
+    protected void OnPropertyChanging([CallerMemberName] string propertyName = "")
+        => PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+    protected abstract void OnThemeChanging(AppTheme newValue);
+}
