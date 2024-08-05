52923d634c6dc631b45121b770c11c65be744eea
ElectroAttacks <casparjacobweber@gmail.com>
2024-08-05 21:45:15 +0200

    Introduced a new internal interface `ISetting` in the `ModernUI.Hosting.Contracts.Models` namespace. This interface specifies the contract for a setting that notifies when its value changes and can be serialized. It includes two properties: `IsRegistered` and `IsDefaultValue`.

---
+++ ModernUI.Hosting/Contracts/Models/ISetting.cs
@@ -0,0 +1,18 @@
+﻿namespace ModernUI.Hosting.Contracts.Models;
+
+/// <summary>
+///     Specifies the contract for a setting that notifies when its value changes and can be serialized.
+/// </summary>
+internal interface ISetting
+{
+
+    /// <summary>
+    ///     Gets whether the setting is registered in the cache.
+    /// </summary>
+    bool IsRegistered { get; }
+
+    /// <summary>
+    ///     Gets whether the settings value equals <see langword="default"/>.
+    /// </summary>
+    bool IsDefaultValue { get; }
+}
