00b6e196d93d1727ac239ce65bad10c624f709bf
ElectroAttacks <casparjacobweber@gmail.com>
2024-08-05 21:41:49 +0200

    Add `INavigableView<TViewModel>` interface to specify the contract for navigable views. The interface is generic with a type parameter `TViewModel` representing the view-model type. Include a read-only property `ViewModel` of type `TViewModel` to get the associated view-model.

---
+++ ModernUI.Abstractions/Contracts/Models/INavigableView.cs
@@ -0,0 +1,16 @@
+﻿namespace ModernUI.Abstractions.Contracts.Models;
+
+/// <summary>
+///     Specifies the contract for navigable views.
+/// </summary>
+/// <typeparam name="TViewModel">
+///     The type of the view-model associated with the view.
+/// </typeparam>
+public interface INavigableView<TViewModel>
+{
+
+    /// <summary>
+    ///     Gets the view-model associated with the view.
+    /// </summary>
+    TViewModel ViewModel { get; }
+}
