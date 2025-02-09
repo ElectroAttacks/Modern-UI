0d282c116846cefab9ac070a387b133c9d4899c8
ElectroAttacks <casparjacobweber@gmail.com>
2025-02-09 13:20:33 +0100

    Add LocalSettingService for persistent settings management
    This commit introduces the `LocalSettingService` class, which implements the `ILocalSettingService` interface. The class is designed to persist and restore settings using a concurrent dictionary. It features asynchronous methods for adding, retrieving, and removing settings, as well as logic to determine which settings should be persisted. Additionally, a private method is included to convert category and key names to JSON format based on specified options. The implementation leverages various namespaces for logging, configuration, and JSON serialization, improving overall functionality and integration.

---
+++ ModernUI.Hosting/Services/LocalSettingService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using ModernUI.Hosting.Contracts.Models;
+using ModernUI.Hosting.Contracts.Services;
+using ModernUI.Hosting.Models;
+using ModernUI.Hosting.Services.Abstractions;
+
+namespace ModernUI.Hosting.Services;
+
+/// <summary>
+///     Provides a service to persist and restore settings.
+/// </summary>
+public sealed class LocalSettingService : PersistAndRestoreService<LocalSettingService, string, ConcurrentDictionary<string, object>>, ILocalSettingService
+{
+
+    #region Constructors
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="LocalSettingService"/> class.
+    /// </summary>
+    /// <inheritdoc/>
+    public LocalSettingService(ILogger<LocalSettingService> logger, IFileService fileService, IConfiguration configuration, Action<ReadOnlyPersistAndRestoreService<LocalSettingService, string, ConcurrentDictionary<string, object>>>? setup = null)
+        : base(logger, fileService, configuration, setup)
+    {
+    }
+
+    #endregion
+
+
+    /// <inheritdoc/>
+    public async Task<LocalSetting<T>> GetOrAddAsync<T>(string category, string key, T? initialValue = default, CancellationToken cancellationToken = default)
+    {
+        await WaitForCompletionAsync(_restoreCompletionSource, "restoring")
+            .ConfigureAwait(false);
+
+        using IDisposable writeLock = await _readerWriterLock.WriterLockAsync(cancellationToken).ConfigureAwait(false);
+
+        ConvertToJsonName(ref category, ref key);
+
+        if (!_data.TryGetValue(category, out ConcurrentDictionary<string, object>? settings))
+        {
+            settings = new ConcurrentDictionary<string, object>();
+
+            _data[category] = settings;
+        }
+
+        if (!settings.TryGetValue(key, out object? setting) || setting is not LocalSetting<T>)
+        {
+            // When the JSON Serializer cannot create a type, it returns a JsonElement.
+            if (setting is JsonElement jsonElement)
+                setting = jsonElement.Deserialize<LocalSetting<T>>(_jsonOptions);
+
+            setting ??= new LocalSetting<T>(initialValue);
+
+            settings[key] = setting;
+        }
+
+        return (LocalSetting<T>)setting;
+    }
+
+
+    /// <inheritdoc/>
+    public async Task<bool> RemoveAsync(string category, string key, CancellationToken cancellationToken = default)
+    {
+        await WaitForCompletionAsync(_restoreCompletionSource, "restoring")
+            .ConfigureAwait(false);
+
+        using IDisposable writeLock = await _readerWriterLock.WriterLockAsync(cancellationToken).ConfigureAwait(false);
+
+        ConvertToJsonName(ref category, ref key);
+
+        return _data.TryGetValue(category, out ConcurrentDictionary<string, object>? settings) && settings.TryRemove(key, out _);
+    }
+
+
+    /// <inheritdoc/>
+    protected override bool ShouldPersist(KeyValuePair<string, ConcurrentDictionary<string, object>> entry)
+    {
+        var filteredSettings = new ConcurrentDictionary<string, object>();
+
+        foreach ((string key, object setting) in entry.Value)
+        {
+            if (setting is not ILocalSetting { IsDefaultValue: true })
+                filteredSettings[key] = setting;
+        }
+
+        return !filteredSettings.IsEmpty;
+    }
+
+
+    /// <summary>
+    ///     Converts the category and key to the JSON name.
+    /// </summary>
+    /// <param name="category">
+    ///     The category to convert.
+    /// </param>
+    /// <param name="key">
+    ///     The key to convert.
+    /// </param>
+    private void ConvertToJsonName(ref string category, ref string key)
+    {
+        category = _jsonOptions.DictionaryKeyPolicy?.ConvertName(category) ?? category;
+        key = _jsonOptions.DictionaryKeyPolicy?.ConvertName(key) ?? key;
+    }
+
+}
