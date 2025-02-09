525370635c7bbd90edd4b4b4c1e7e92fc33e9478
ElectroAttacks <casparjacobweber@gmail.com>
2025-02-09 13:18:41 +0100

    Add ILocalSetting interface with IsDefaultValue property
    Introduces the `ILocalSetting` interface in the
    `ModernUI.Hosting.Contracts.Models` namespace.
    This interface includes a property `IsDefaultValue`
    to indicate if the setting's value is the default.
    XML documentation comments are also added for clarity.

---
+++ ModernUI.Hosting/Contracts/Models/ILocalSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace ModernUI.Hosting.Contracts.Models;
+
+/// <summary>
+///     Specifies the contract for a setting that notifies when its value changes and can be serialized.
+/// </summary>
+public interface ILocalSetting
+{
+
+    /// <summary>
+    ///     Gets whether the settings value equals <see langword="default"/>.
+    /// </summary>
+    bool IsDefaultValue { get; }
+}
